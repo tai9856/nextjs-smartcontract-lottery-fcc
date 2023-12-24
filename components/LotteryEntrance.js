177d46ce4e7aa00a9b28bf9dae7ddb3da047eaef
tai9856 <tai981567@gmail.com>
2023-12-24 16:07:11 +0700

    message

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -17,7 +17,11 @@ export default function LotteryEntrance() {
 
     const dispatch = useNotification()
 
-    const { runContractFunction: enterRaffle } = useWeb3Contract({
+    const {
+        runContractFunction: enterRaffle,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "enterRaffle",
@@ -60,7 +64,18 @@ export default function LotteryEntrance() {
             updateUI()
         }
     }, [isWeb3Enabled])
+    // no list means it'll update everytime anything changes or happens
+    // empty list means it'll run once after the initial rendering
+    // and dependencies mean it'll run whenever those things in the list change
 
+    // An example filter for listening for events, we will learn more on this next Front end lesson
+    // const filter = {
+    //     address: raffleAddress,
+    //     topics: [
+    //         // the name of the event, parnetheses containing the data type of each event, no spaces
+    //         utils.id("RaffleEnter(address)"),
+    //     ],
+    // }
     const handleSuccess = async function (tx) {
         await tx.wait(1)
         handleNewNotification(tx)
@@ -77,23 +92,29 @@ export default function LotteryEntrance() {
     }
 
     return (
-        <div>
+        <div className="p-5">
             Hi from LotteryEntrance!
             {raffleAddress ? (
                 <div>
                     <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
                         onClick={async function () {
                             await enterRaffle({
                                 onSuccess: handleSuccess,
                                 onError: (error) => console.log(error),
                             })
                         }}
+                        disabled={isFetching || isLoading}
                     >
-                        Enter Raffle
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                        ) : (
+                            <div>Enter Raffle</div>
+                        )}
                     </button>
-                    Entrance Fee: {ethers.formatEther(entranceFee)} ETH Number of players:
-                    {numberOfPlayers}
-                    recentWinner: {recentWinner}
+                    <div>Entrance Fee: {ethers.formatEther(entranceFee)}</div>
+                    <div>ETH Number of players: {numberOfPlayers}</div>
+                    <div>Recent Winner: {recentWinner}</div>
                 </div>
             ) : (
                 <div>No Raffle Address Detached!</div>
