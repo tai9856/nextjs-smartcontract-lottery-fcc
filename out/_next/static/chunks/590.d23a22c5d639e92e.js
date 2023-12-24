617120290761f6bd9a8c9caa6d1320904e058283
tai9856 <tai981567@gmail.com>
2023-12-24 17:46:06 +0700

    my changes

---
+++ out/_next/static/chunks/590.d23a22c5d639e92e.js
@@ -0,0 +1,16 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[590],{62590:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var c=r(41942);r(67294),r(83078);let{ButtonTranslucentStyled:n}={ButtonTranslucentStyled:(0,c.s)(c.B)`
+    background-color: ${(0,c.g)("dark",20)};
+    color: ${c.c.white};
+
+    :active {
+        border: 2px solid transparent;
+    }
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${c.c.paleCerulean};
+    }
+
+    svg {
+        fill: ${c.c.white};
+    }
+`},a=({...e})=>(0,c.j)(n,{...e})}}]);
